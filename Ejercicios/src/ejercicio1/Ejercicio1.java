6e86df68a7fc162ad5c084f09ec5fc58735364b3
javier <javiierespejoo@gmail.com>
2023-09-26 12:40:01 +0200

    commit ejercicios ad 26/09/23

--- Ejercicios/src/ejercicio1/Ejercicio1.java
+++ Ejercicios/src/ejercicio1/Ejercicio1.java
@@ -1,6 +1,7 @@
 package ejercicio1;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class Ejercicio1 {
 
@@ -11,7 +12,25 @@ public class Ejercicio1 {
 		File d1 = new File("C:\\Users\\alu\\eclipse-workspace\\ejemplosAD\\Ficheros");
 		File d2 = new File("C:\\Users\\alu\\eclipse-workspace\\ejemplosAD\\Javier");
 		
+		ArrayList<File> listaFiles = new ArrayList<>();
+		listaFiles.add(f1);
+		listaFiles.add(f2);
+		listaFiles.add(d1);
+		listaFiles.add(d2);
 		
+		for (File file : listaFiles) {
+				if(file.exists()) {
+					if(file.isDirectory()) {
+						System.out.println("El directorio " + file.getName() + ", existe.");
+					} else {
+						System.out.println("EL fichero " + file.getName() + ", existe.");
+					}
+				} else {
+					System.out.println(file.getName() + " NO existe.");
+				}
+				System.out.println(" Y su path es: " + file.getPath());
+			
+		}
 
 	}
 
